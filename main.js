4f58ea188e7e686c7765e4e4b5ba1af2e8e3688d
LUIZ HENRIQUE DOS SANTOS <24122470@CFP127ALUNO.LOCAL>
2025-03-18 09:47:33 -0300

    README OK

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 "use strict"
-const botaoTrocaImagem = document.getElementById('trocar-imagem')
+const botaoTrocaImagem = document.getElementById('trocarImagem');
 
-function trocarImagem () {
-    const imagem = document.getElementById('imagem').value
-    document.documentElement.style.setProperty('--cor-fundo', imagem)
+
+
+function trocarImagem(){
+    const body = document.querySelector('body');
+
+    const imagens = [
+        'var(--imagem1)',
+        'var(--imagem2)',
+        'var(--imagem3)',
+        'var(--imagem4)',
+        'var(--imagem5)'
+      ];
+
+    let fundoAtual = 0;
+
+
+    return function(){
+        fundoAtual = (fundoAtual + 1) % imagens.length;
+        body.style.backgroundImage = imagens[fundoAtual];    
+    }
+    
 }
 
-botaoTrocaCor.addEventListener('click', trocarImagem)
+const mudarFundo = trocarImagem()
+
+botaoTrocaImagem.addEventListener('click', mudarFundo)
